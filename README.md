fa592b1b006fd9300c438752c09fd87ea49e4d0b
Mike Wallio <michael.wallio@outlook.com>
2022-02-10 09:07:49 -0500

    Initial implementation
    - Add unvm.py
      - Uses the index.json from unofficial builds to figure out what available
      - Puts together a download URL based on selection
      - Hopefully manages symbolic links
    - Add unvm.bat for ease of use on path

---
+++ README.md
@@ -0,0 +1,37 @@
+# Unofficial Node Version Manager
+
+A utility around [nodejs unofficial-builds](https://unofficial-builds.nodejs.org/download/release/) for Windows on arm64.
+
+## Options
+
+```sh
+  -h, --help            show this help message and exit
+  --install             Install a specific version of node
+  --available           List available node versions to install
+  --use                 Switch to an installed node version
+  --installed           List installed versions of node
+```
+
+## Requirements
+
+Enable developer mode in Windows Settings.
+
+Update your environment PATH variable to include:
+
+Path to where this was cloned (so you can type unvm anywhere):
+
+```cmd
+%USERPROFILE%\GitHub\unvm
+```
+
+For full Python install (so node can be found on path):
+
+```cmd
+%LOCALAPPDATA%\unvm\node
+```
+
+For Windows Store Python 3.10 install (so node can be found on path):
+
+```cmd
+%LOCALAPPDATA%\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\Local\unvm\node
+```
---
+++ unvm.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python3
+
+import argparse
+import io
+import json
+import os
+import requests
+import zipfile
+
+root_dir = os.path.join(os.environ['LOCALAPPDATA'], 'unvm')
+installed_dir = os.path.join(root_dir, 'installed')
+current_dir = os.path.join(root_dir, 'node')
+
+def get_manifest():
+    response = requests.get('https://unofficial-builds.nodejs.org/download/release/index.json')
+    if not response.ok:
+        print('Could not load build manifest')
+        exit(-1)
+
+    return json.loads(response.content)
+
+def filter_entries(manifest):
+    return [item for item in manifest if 'win-arm64-zip' in item['files']]
+
+def installed():
+    return [folder.removeprefix('node-').removesuffix('-win-arm64') for folder in os.listdir(installed_dir)]
+
+def install(version, manifest):
+    if version == 'lts':
+        version = next(filter(lambda item: item['lts'], manifest))['version']
+    
+    target = 'https://unofficial-builds.nodejs.org/download/release/' + version + '/node-' + version + '-win-arm64.zip'
+    file_response = requests.get(target)
+    if not file_response.ok:
+        print('Failed to download nodejs')
+        exit(-1)
+    
+    file = io.BytesIO(file_response.content)
+    os.makedirs(installed_dir, exist_ok=True)
+    with zipfile.ZipFile(file, 'r') as archive:
+        archive.extractall(installed_dir)
+
+    if os.path.exists(current_dir):
+        print(version + ' installed')
+        print('To use:')
+        print('unvm --use ' + version)
+    else:
+        use(version)
+
+def use(version):
+    os.chdir(root_dir)
+    os.unlink(os.path.join('.', 'node'))
+    os.symlink(os.path.join('.', 'installed', 'node-' + version + '-win-arm64'), os.path.join('.', 'node'))
+    print('Now using node ' + version)
+
+manifest = filter_entries(get_manifest())
+available = ['lts'] + [item['version'] for item in manifest]
+
+parser = argparse.ArgumentParser(description='Manage node installs on Windows arm64')
+parser.add_argument('--install', choices=available, help='Install a specific version of node')
+parser.add_argument('--available', default=False, action='store_true', help='List available node versions to install')
+parser.add_argument('--use', type=str, choices=installed(), help='Switch to an installed node version')
+parser.add_argument('--installed', default=False, action='store_true', help='List installed versions of node')
+
+
+args = parser.parse_args()
+
+if args.available:
+    print(available)
+
+if args.installed:
+    print(installed())
+
+if args.install:
+    install(args.install, manifest)
+
+if args.use:
+    use(args.use)
